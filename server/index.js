746b8394898e2c0331dc1e833adc28429dbc6721
dokalolc <jigmetdolkar18@gmail.com>
2026-06-02 12:18:40 +0530

    final final done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+
 const app = express();
 
 const reviewRoutes = require("./routes/reviewRoutes");
